731d97d77b707138c4b24030b29fa04126d87fed
viniblack <viniibl4ck@gmail.com>
2023-05-02 19:03:56 -0300

    section 2 lesson 1

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,45 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
+import { useState } from "react";
 
 const Home = () => {
+  const [userInput, setUserInput] = useState("");
+
+  const onUserChangedText = (event) => {
+    console.log(event.target.value);
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>GPT-3 Writer | WEB3DEV</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1>Converse com o  Goku, do Dragon Ball</h1>
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2>Escreva uma mensagem para o Goku e pergunte a ele sobre qualquer coisa.</h2>
+          </div>
+        </div>
+        {/* Adicione esse código aqui */}
+        <div className="prompt-container">
+          <textarea
+            className="prompt-box"
+            placeholder="digite aqui..."
+            value={userInput}
+            onChange={onUserChangedText}
+          />
+          {/* Novo código que adicionei aqui */}
+          <div className="prompt-buttons">
+            <a className="generate-button" onClick={null}>
+              <div className="generate">
+                <p>Gerar</p>
+              </div>
+            </a>
           </div>
         </div>
       </div>
